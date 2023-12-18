a02422563fa1bde89b19c31db404304d91d9e2ff
Grant Orchard <go@hashicorp.com>
2023-12-19 08:34:38 +1100

    update resource id for import

--- main.tf
+++ main.tf
@@ -136,6 +136,7 @@ resource "aws_api_gateway_resource" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id = aws_api_gateway_rest_api.this.root_resource_id
   path_part = "{proxy+}"
+  id = "pskqpjxiuf/cioblx"
 }
 
 
