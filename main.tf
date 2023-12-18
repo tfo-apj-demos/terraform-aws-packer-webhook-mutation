1c7647ca1325444d479eea450b5309674dbfcbac
Grant Orchard <go@hashicorp.com>
2023-12-19 08:54:13 +1100

    update resource id for import

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_api_gateway_rest_api" "this" {
 # }
 
 import {
-  id = "pskqpjxiuf/cioblx"
+  id = "t8j4t4inja/cioblx"
   to = aws_api_gateway_resource.this
 }
 resource "aws_api_gateway_resource" "this" {
