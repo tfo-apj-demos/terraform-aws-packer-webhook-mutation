9b2676f485a284abe1011e3572c8250d132ca7be
Grant Orchard <go@hashicorp.com>
2023-12-19 08:26:24 +1100

    update resource id for import

--- main.tf
+++ main.tf
@@ -117,24 +117,24 @@ resource "aws_api_gateway_rest_api" "this" {
 #   stage_name    = "this"
 # }
 
-import {
-  id = "t8j4t4inja/pskqpjxiuf"
-  to = aws_api_gateway_resource.root
-}
+# import {
+#   id = "t8j4t4inja/pskqpjxiuf"
+#   to = aws_api_gateway_resource.root
+# }
 
-resource "aws_api_gateway_resource" "root" {
-  rest_api_id = "t8j4t4inja"
-  parent_id = "t8j4t4inja"
-  path_part = "/"
-}
+# resource "aws_api_gateway_resource" "root" {
+#   rest_api_id = "t8j4t4inja"
+#   parent_id = "t8j4t4inja"
+#   path_part = "/"
+# }
 
 import {
   id = "pskqpjxiuf/cioblx"
   to = aws_api_gateway_resource.this
 }
 resource "aws_api_gateway_resource" "this" {
-  rest_api_id = "t8j4t4inja"
-  parent_id = "pskqpjxiuf"
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id = aws_api_gateway_rest_api.this.root_resource_id
   path_part = "{proxy+}"
 }
 
