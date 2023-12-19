b90076011059a3a5379f0d36d0e12c576b4ada24
Grant Orchard <go@hashicorp.com>
2023-12-19 15:27:05 +1100

    add explicit dependency for deployment

--- 12 - api_gateway.tf
+++ 12 - api_gateway.tf
@@ -19,7 +19,7 @@ resource "aws_api_gateway_deployment" "this" {
 resource "aws_api_gateway_stage" "this" {
   deployment_id = aws_api_gateway_deployment.this.id
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  stage_name    = "test"
+  stage_name    = "default"
 
 }
 
